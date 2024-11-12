7548607d584527aadfe4b58fa63fd66ab8034822
Alex Bril <aleksey.bril@renlife.com>
2024-11-12 19:41:47 +0300

    Update

--- src/main/java/com/renlife/cloud/demo/DemoApplication.java
+++ src/main/java/com/renlife/cloud/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.renlife.cloud.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
